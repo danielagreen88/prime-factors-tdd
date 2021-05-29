8e39bf256846726495476e8b1f05e0957d965092
Daniela Green <daniela.grin@gmail.com>
2021-05-29 20:17:16 +0300

    added prettier config

--- src/PrimeFactors.test.js
+++ src/PrimeFactors.test.js
@@ -1,6 +1,6 @@
-import { PrimeFactors } from "./PrimeFactors.js";
+import { PrimeFactors } from './PrimeFactors.js';
 
-test("should calculate prime factors of 1", () => {
+test('should calculate prime factors of 1', () => {
   const primeFactors = new PrimeFactors();
   expect(primeFactors.calc(1)).toEqual([]);
 });
